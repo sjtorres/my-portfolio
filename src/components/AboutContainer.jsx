e32f5691c846e701e7289428a67f9bb6a7e88e9f
Sandro Torres <sandrotorres.ti@gmail.com>
2022-10-23 06:40:15 -0300

    Main Content atualizado

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,11 @@
+import '../styles/components/maincontent.sass'
+
+const AboutContainer = () => {
+  return <section className="about-container">
+    <h2>Sobre</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime nam quae sapiente provident et quos magni voluptatum eligendi cum fugiat ea porro quis voluptatem similique, neque commodi beatae distinctio temporibus.</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat iure dolores soluta qui odio iste accusantium atque excepturi, quibusdam recusandae, ullam saepe nam facere, ea vitae velit labore nostrum nobis.</p>
+  </section>
+}
+
+export default AboutContainer
---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectsContainer = () => {
+  return <section className="projects-container">
+    <h2>Projetos</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda similique quisquam repellat tempore illo eos officiis cum at, quis officia, odio, ratione quos blanditiis quas. Eaque est nulla nam reprehenderit!</p>
+    <a href="https://github.com/sjtorres" className="btn" target='_blank'>Ver Projetos</a>
+  </section>
+}
+
+export default ProjectsContainer
